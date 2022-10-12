701ecc89f52e08d2f3833c133e1ea7224c3ab3b4
Adriano Maurmann <adriano.maurmann@gmail.com>
2022-10-12 14:26:22 -0300

    initial

---
+++ BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿namespace dotnet_algorithms
+{
+    public class BubbleSort
+    {
+        public void Sort(int[] values)
+        {
+            if (values == null || values.Length < 2)
+                return;
+
+            var length = values.Length;
+
+            while (length > 0)
+            {
+                for (int index = 1; index < length; index++)
+                {
+                    if (values[index] < values[index - 1])
+                    {
+                        var greatValue = values[index];
+                        values[index] = values[index - 1];
+                        values[index - 1] = greatValue;
+                    }
+                }
+                length--;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using dotnet_algorithms;
+using System.Diagnostics;
+
+var values = new int[] { 34,342,3,0,15,8,9,0 };
+
+Console.WriteLine($"Unsorted...: {string.Join(',', values)}");
+
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+new BubbleSort().Sort(values);
+stopWatch.Stop();
+
+Console.WriteLine($"Sorted.....: {string.Join(',', values)}");
+Console.WriteLine($"Elapsed ms.: {stopWatch.ElapsedMilliseconds}");
+Console.ReadKey();
